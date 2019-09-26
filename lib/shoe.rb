9fb68da1af5260a5e540ce7b2d75160d69f50b56
Damini Varu <damvaru4@gmail.com>
2019-09-26 13:56:17 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,2 +1,49 @@
 # Make your shoe class here!
+class Show
+  
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+   
+  def initialize(brand)
+    @brand = brand
+    end
+  
+  def title
+    @title
+    end 
+    
+    def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+ 
+ def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end 
+
+
+
+Show.new("Adidas")
+
  
